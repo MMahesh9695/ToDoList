eb25159c3ddaf5c7783cadecd618eecb9915cce0
Maheshwaran <tmmaheshwaran@gmail.com>
2021-08-16 00:22:44 +0530

    added drag and drop feature

--- toDoList.js
+++ toDoList.js
@@ -1,24 +1,34 @@
 var btnAdd = document.querySelector('#btn-add');
-
-var themeColor_0 = document.querySelector('.theme-0');
-var themeColor_1 = document.querySelector('.theme-1');
-var themeColor_2 = document.querySelector('.theme-2');
-var themeColor_3 = document.querySelector('.theme-3');
-var themeColor_4 = document.querySelector('.theme-4');
-
-var selectedTheme = "theme-0";
-
 btnAdd.addEventListener('click', btnAddClick);
-themeColor_0.addEventListener('click', () =>changeTheme('theme-0'));
-themeColor_1.addEventListener('click', () =>changeTheme('theme-1'));
-themeColor_2.addEventListener('click', () =>changeTheme('theme-2'));
-themeColor_3.addEventListener('click', () =>changeTheme('theme-3'));
-themeColor_4.addEventListener('click', () =>changeTheme('theme-4'));
 
+var defaultTheme = "theme-0";
+let txtDetails = document.querySelector('#txt-details');
+var counter =1;
+//// Without Event delegation
+// var themeColor_0 = document.querySelector('.theme-0');
+// var themeColor_1 = document.querySelector('.theme-1');
+// var themeColor_2 = document.querySelector('.theme-2');
+// var themeColor_3 = document.querySelector('.theme-3');
+// var themeColor_4 = document.querySelector('.theme-4');
+
+// themeColor_0.addEventListener('click', () =>changeTheme('theme-0'));
+// themeColor_1.addEventListener('click', () =>changeTheme('theme-1'));
+// themeColor_2.addEventListener('click', () =>changeTheme('theme-2'));
+// themeColor_3.addEventListener('click', () =>changeTheme('theme-3'));
+// themeColor_4.addEventListener('click', () =>changeTheme('theme-4'));
+
+//Event Delegation
+const themeList = document.querySelector('.ul_themelist');
+themeList.addEventListener('click',(e)=>{
+    if(e.target.tagName == "DIV"){
+        changeTheme(e.target.classList[1]);
+    }
+});
 
 function changeTheme(themeClass){
-    selectedTheme = themeClass;
-    document.querySelector('#txt-details').className = themeClass;
+    defaultTheme = themeClass;
+    txtDetails.classList.remove(...txtDetails.classList);
+    txtDetails.classList.add(themeClass,"text-box");
 }
 
 function btnAddClick(){
@@ -29,12 +39,38 @@ function btnAddClick(){
         return false;
     }
 
-    var newList = document.createElement('li');
-    var txtNode = document.createTextNode(enteredText);
-    newList.appendChild(txtNode);
-    newList.classList.add('todolist-items', selectedTheme);
-    document.querySelector('#todo-items').appendChild(newList);
+    let todolistDiv = document.createElement('div');
+    todolistDiv.classList.add("todolist-items",defaultTheme);
+    todolistDiv.setAttribute('draggable',true);
+    todolistDiv.setAttribute('id','listItems'+ ++counter);
+    todolistDiv.append(enteredText);
+    todolistDiv.addEventListener('dragstart',dragStartList);
+
+    document.querySelector('#toDoItem').appendChild(todolistDiv);
     document.querySelector('#txt-details').value = "";
-    selectedTheme = "theme-0"
-    document.querySelector('#txt-details').className = selectedTheme;
+    txtDetails.classList.remove(...txtDetails.classList);
+    defaultTheme = "theme-0";
+    document.querySelector('#txt-details').classList.add(defaultTheme,"text-box");
+}
+
+
+const completedList = document.querySelector('#completedList');
+
+completedList.addEventListener('dragover',dragOverList);
+completedList.addEventListener('drop',dragDropList);
+
+function dragStartList(e){
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+function dragOverList(e){
+    e.preventDefault();
+    console.log('c');
+}
+
+function dragDropList(e){
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    console.log(data);
+    e.target.appendChild(document.getElementById(data));
 }
